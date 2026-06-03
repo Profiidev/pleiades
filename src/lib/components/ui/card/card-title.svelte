0e4ff17df5609c51124a45802e612af04a401222
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-06-03 13:52:12 +0200

    chore: updated components

--- src/lib/components/ui/card/card-title.svelte
+++ src/lib/components/ui/card/card-title.svelte
@@ -14,7 +14,7 @@
   bind:this={ref}
   data-slot="card-title"
   class={cn(
-    'text-base leading-snug font-medium group-data-[size=sm]/card:text-sm',
+    'font-heading text-base leading-snug font-medium group-data-[size=sm]/card:text-sm',
     className
   )}
   {...restProps}
